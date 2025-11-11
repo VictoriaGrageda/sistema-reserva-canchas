1861c4ff94b8451db55e3d56173f86b4b3217381
alejandro <alejandrotorrico743@gmail.com>
2025-11-11 00:27:58 -0400

    fix: cleanup env and node_modules tracking

--- frontend/src/api/http.ts
+++ frontend/src/api/http.ts
@@ -3,7 +3,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const http = axios.create({
   //ale
-  baseURL: process.env.EXPO_PUBLIC_API_URL ?? "http://192.168.100.56:3000/api/v1",
+  baseURL: process.env.EXPO_PUBLIC_API_URL ?? "http://192.168.100.13:3000/api/v1",
   //brandon
   // baseURL: process.env.EXPO_PUBLIC_API_URL ?? "http://192.168.0.11:3000/api/v1",
   timeout: 10000,
